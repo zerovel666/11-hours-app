36ea37f7022b9b6070e21465b031c240fbe717b1
zerovel666 <zerovel060606@gmail.com>
2025-01-10 00:22:03 +0500

    urlncode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,9 +1,16 @@
+from django.utils.http import urlencode
 from django import template
 from goods.models import Categories
 
 
 register = template.Library()
 
-@register.simple_tag
+@register.simple_tag()
 def tag_categories():
     return  Categories.objects.all()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
