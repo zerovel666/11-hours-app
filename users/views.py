b87dccd8e0169dd4ea9f20f76cf98c50bb753e98
zerovel666 <zerovel060606@gmail.com>
2025-01-11 17:29:56 +0500

    add to cart view

--- users/views.py
+++ users/views.py
@@ -17,6 +17,9 @@ def login(request):
             if user:
                 auth.login(request,user)
                 messages.success(request,f"{username}, Вы вошли в аккаунт")
+                redirect_page = request.POST.get('next',None)
+                if redirect_page and redirect_page != reverse('user:logout'):
+                    return HttpResponseRedirect(request.POST.get('next'))
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
